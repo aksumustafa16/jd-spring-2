f1ac307f2bafa789379d6acd57e650552c17c7cd
musaksu <musaksu@gmail.com>
2023-01-18 22:26:22 +0100

    Post Construct

--- Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -1,10 +1,10 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
-import org.springframework.stereotype.Component;
 
 public class Selenium implements Course {
+    @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours 22");
+        System.out.println("Teaching hours : 20");
     }
 }
