d20dbdc1179e44dac406f366f3af6a5919c500d6
musaksu <musaksu@gmail.com>
2023-01-19 00:29:00 +0100

    Spring Boot Demo]

--- Spring-07-Springboot-Demo/src/main/java/com/cybertek/Spring07SpringbootDemoApplication.java
+++ Spring-07-Springboot-Demo/src/main/java/com/cybertek/Spring07SpringbootDemoApplication.java
@@ -1,13 +1,20 @@
 package com.cybertek;
 
+import com.cybertek.interfaces.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring07SpringbootDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring07SpringbootDemoApplication.class, args);
+        ApplicationContext container = SpringApplication.run(Spring07SpringbootDemoApplication.class, args);
+
+        Course course = container.getBean("java",Course.class);
+
+        System.out.println(course.getTeachingHours());
+
     }
 
 }
